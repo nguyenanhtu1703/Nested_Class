f41ee12bb1ab9a766fb9cc75baec0d5bd2fd2929
nguyenanhtu2 <nguyenanhtuinc@gmail.com>
2019-07-31 12:56:28 +0200

    second commit

--- src/App.java
+++ src/App.java
@@ -1,30 +1,60 @@
 public class App {
 	public static void main(String[] args) {
 		CarParts carParts = new CarParts();
-		CarParts.Wheel wheel = new CarParts.Wheel();
-		CarParts.Wheel.InnerWheel InnerWheel = wheel.new InnerWheel();
+		System.out.println(CarParts.Wheel.InnerWheel.me);
+		A.ok();
 	}
 }
 
 class CarParts {
 	public static int factoryID = 123;
-	public static class Wheel {
-		public static int factoryInnerID = 5;
+	public class Wheel {
+		public static final int factoryInnerID = 5;
 		public Wheel() {
+			System.out.println("Wheel factory ID: " + AnotherWheel.factoryInnerID2);
+			ShowStaticMethodOfEnclosingClass();
+		}
+		public class InnerWheel {
+			public static final int me = 8;
+			public InnerWheel() {
+				System.out.println("Inner wheel " + factoryID + " " + AnotherWheel.factoryInnerID2);
+				ShowStaticMethodOfEnclosingClass();
+			}
+			public void Ok() {
+			}
+		}
+	}
+	
+	public class AnotherWheel {
+		public static final int factoryInnerID2 = 5;
+		public AnotherWheel() {
 			System.out.println("Wheel factory ID: " + factoryID);
 			ShowStaticMethodOfEnclosingClass();
 		}
 		public class InnerWheel {
+			public static final int me = 8;
 			public InnerWheel() {
-				System.out.println("Inner wheel " + factoryID + " " + factoryInnerID);
+				System.out.println("Inner wheel " + factoryID + " " + Wheel.factoryInnerID);
 				ShowStaticMethodOfEnclosingClass();
+			}
+			public void Ok() {
 			}
 		}
 	}
+	
 	public CarParts() {
 		System.out.println("Car parts object created!");
 	}
 	public static void ShowStaticMethodOfEnclosingClass() {
 		System.out.println("yes");
+	}
+}
+
+interface A {
+	public static void ok() {
+		System.out.println("lol");
+	}
+	public default void OK() {	
+		System.out.println("LOL");
 	}
 }
